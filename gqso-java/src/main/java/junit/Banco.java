4911c6d8f79844a6fae606668b935fd96a105c3c
Micledson Carlos <micledson14@gmail.com>
2021-04-07 14:14:20 -0300

    add class exception SaldoInsuficiente

--- gqso-java/src/main/java/junit/Banco.java
+++ gqso-java/src/main/java/junit/Banco.java
@@ -3,6 +3,10 @@ package junit;
 public class Banco {
     private double saldo;
 
+    public class SaldoInsuficiente extends Exception{
+        private static final long serialVersionUID = 1L;
+    }
+
     public double saque(double valor) {
         setSaldo(getSaldo() - valor);
         return getSaldo();
