f7d548422555cc60ed15218ec878aefe77b7c554
Micledson Carlos <micledson14@gmail.com>
2021-04-07 14:03:34 -0300

    add metodo testeSaque na classe BancoTeste

--- gqso-java/src/test/java/junit/BancoTeste.java
+++ gqso-java/src/test/java/junit/BancoTeste.java
@@ -1,5 +1,19 @@
 package junit;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class BancoTeste {
-    
+    Banco banco;
+
+    @BeforeEach
+    public void setUp() {
+        banco = new Banco();
+    }
+
+    @Test
+    public void testeSaque() {
+        
+    }
+
 }
